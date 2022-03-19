1e2b7411174540806d4abc41d9f1fe1725e12b06
Бабич Никита Сергеевич гр. ИС(АБД)-41 <ta12415@stud.KHPK.RU>
2022-03-19 09:08:35 +0700

    Отправка 1

---
+++ Kursovaya/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursovaya
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSelectVictim_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSelectAccused_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kursovaya/Windows/AddCaseWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursovaya.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCaseWindow.xaml
+    /// </summary>
+    public partial class AddCaseWindow : Window
+    {
+        PoliceEntities context;
+        public AddCaseWindow(PoliceEntities context)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbJudge.ItemsSource = context.Judges.ToList();
+            CmbAccused.ItemsSource = context.Accuseds.ToList();
+            CmbVictim.ItemsSource = context.Victims.ToList();
+            //this.DataContext = case;
+        }
+
+        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            MessageBox.Show("Данные добавлены");
+            this.Close();
+        }
+    }
+}
